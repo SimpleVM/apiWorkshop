73df6427976dfe6b80a60d63d1bef5af419caa7e
dweinholz <david-weinholz@web.de>
2025-09-30 17:15:39 +0200

    Update data.sh

--- data.sh
+++ data.sh
@@ -1,1 +1,1 @@
-wget -P ~/ https://openstack.cebitec.uni-bielefeld.de:8080/ftp.era.ebi.ac.uk/vol1/fastq/DRR127/DRR127583/DRR127583_1.fastq.gz
+wget -P /home/ubuntu/ https://openstack.cebitec.uni-bielefeld.de:8080/ftp.era.ebi.ac.uk/vol1/fastq/DRR127/DRR127583/DRR127583_1.fastq.gz
